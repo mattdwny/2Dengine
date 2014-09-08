5ded86120a578fda5106c444eade9fb6bf5c748d
Matt Downey <matthew.r.downey@gmail.com>
2014-09-08 00:57:07 -0400

    This is after alterations to make the code "flowery"

--- graphics.h
+++ graphics.h
@@ -1,12 +1,12 @@
 #ifndef _GRAPHICS_
 #define _GRAPHICS_
-/*
- *			graphics.h
- *			Originaly written by Donald Kehoe for the NJIT Introduction to game development class
- *			This file contains data structures, constants and function prototypes needed to initialize SDL and
- *			to handle sprites and graphics.
- */
 
+/**
+ *	graphics.h
+ *	Originaly written by Donald Kehoe for the NJIT Introduction to game development class
+ *	This file contains data structures, constants and function prototypes needed to initialize SDL and
+ *	to handle sprites and graphics.
+ */
 
 #include "SDL.h"
 #include "SDL_image.h"
@@ -47,7 +47,9 @@
 #define Magenta_     0xFF00FF
 
 
-/*color constants:*/
+/**
+ * color constants:
+ */
 enum colors {Red = 1,Green = 2,Blue = 3,Yellow = 4,Orange = 5,Violet = 6,Brown = 7,Grey = 8,DarkRed = 9,DarkGreen = 10,
                DarkBlue = 11,DarkYellow = 12,DarkOrange = 13,DarkViolet = 14,DarkBrown = 15,DarkGrey = 16,LightRed =17,
                LightGreen = 18,LightBlue = 19,LightYellow = 20,LightOrange = 21,LightViolet = 22,LightBrown = 23,LightGrey = 24,
@@ -55,32 +57,39 @@ enum colors {Red = 1,Green = 2,Blue = 3,Yellow = 4,Orange = 5,Violet = 6,Brown =
 
 typedef struct Sprite_T
 {
-  SDL_Surface *image;		/*pointer to the actual image in memory*/
-  char filename[20];			/*the name of the file, keep them short*/
-  int w, h;					/*the width and height of the frames of the sprites, not the file itself*/
-  int framesperline;			/*default is 16*/
-  int color1,color2,color3;		/*if the file was palette swapped these are the colors that were changed*/
-  int used;					/*used by the maintanenc functions to keep track of how many times a single sprite is being used*/
-}Sprite;
+	SDL_Surface* image;			//pointer to the actual image in memory
+	char filename[20];			//the name of the file, keep them short
+	int w, h;					//the width and height of the frames of the sprites, not the file itself
+	int framesperline;			//default is 16
+	int color1, color2, color3;	//if the file was palette swapped these are the colors that were changed
+	int used;					//used by the maintanenc functions to keep track of how many times a single sprite is being used
+} Sprite;
 
-typedef struct	/*this didn't need to be its own type, its only used once*/
+typedef struct	//this didn't need to be its own type, its only used once
 {
-    int xres,yres,depth;
-}ScreenData;
+	int xres, yres, depth;
+} ScreenData;
 
 void Init_Graphics();
-void DrawPixel(SDL_Surface *screen, Uint8 R, Uint8 G, Uint8 B, int x, int y);
-void BlankScreen(SDL_Surface *buf,Uint32 color);
-void SwapSprite(SDL_Surface *sprite,int color1,int color2,int color3);
+void DrawPixel(SDL_Surface* screen, Uint8 R, Uint8 G, Uint8 B, int x, int y);
+void BlankScreen(SDL_Surface* buf, Uint32 color);
+void SwapSprite(SDL_Surface* sprite, int color1, int color2, int color3);
 void InitSpriteList();
-void FreeSprite(Sprite *img);
-Sprite *LoadSprite(char *filename,int sizex, int sizey);		/*simplest file to load*/
-Sprite *LoadSwappedSprite(char *filename,int sizex, int sizey, int c1, int c2, int c3);
-/*drawsprite and drawspritepart work the same except that Drawsprite will render an entire sprite on the desired srface
-  and drawspritepart can have the size of the sprite specified.  Know your sprite when using DSP since you can in theory
-  read too far, not that SDL will allow that.*/
+void FreeSprite(Sprite* img);
+Sprite* LoadSprite(char* filename, int sizex, int sizey);		//simplest file to load
+Sprite* LoadSwappedSprite(char* filename, int sizex, int sizey, int c1, int c2, int c3);
+
+/**
+ * drawsprite and drawspritepart work the same except that Drawsprite will render an entire sprite on the desired srface
+ * and drawspritepart can have the size of the sprite specified.  Know your sprite when using DSP since you can in theory
+ * read too far, not that SDL will allow that.
+ */
 void DrawSprite(Sprite *sprite,SDL_Surface *surface,int sx,int sy, int frame);
-void CloseSprites();		/*call this before you exit to make sure all the memory that your program used is given back to your OS*/
+
+/**
+ * call this before you exit to make sure all the memory that your program used is given back to your OS
+ */
+void CloseSprites();
 
 Uint32 IndexColor(int color);
 void FrameDelay(Uint32 delay);
